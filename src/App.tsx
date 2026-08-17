5f298370f0e085588ba6d5a80d1c46d8d4a8c5f2
DIMEBIS123123 <villlishely@gmail.com>
2026-08-17 10:27:38 +0300

    Базовые функции

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
 import './App.css'
 
+const tg = window.Telegram.WebApp
+
 function App() {
-	return <div>Фронтенд для бота</div>
+	useEffect(() => {
+		tg.ready()
+	}, [])
+	const onClose = () => {
+		tg.close()
+	}
+	return (
+		<div>
+			<button onClick={onClose}>Закрыть</button>
+		</div>
+	)
 }
 
 export default App
---
+++ src/types/telegram.d.ts
@@ -0,0 +1,45 @@
+export {}
+
+declare global {
+	interface Window {
+		Telegram: {
+			WebApp: {
+				initData: string
+				initDataUnsafe: object
+				version: string
+				platform: string
+				colorScheme: string
+				themeParams: object
+				isExpanded: boolean
+				viewportHeight: number
+				viewportStableHeight: number
+				MainButton: any
+				BackButton: any
+				HapticFeedback: any
+				ready: () => void
+				expand: () => void
+				close: () => void
+				sendData: (data: string) => void
+				enableClosingConfirmation: () => void
+				disableClosingConfirmation: () => void
+				setHeaderColor: (color: string) => void
+				setBackgroundColor: (color: string) => void
+				showAlert: (message: string, callback?: () => void) => void
+				showConfirm: (
+					message: string,
+					callback?: (confirmed: boolean) => void,
+				) => void
+				showPopup: (
+					params: object,
+					callback?: (buttonId: string) => void,
+				) => void
+				switchInlineQuery: (query: string, chooseChatTypes?: string[]) => void
+				openLink: (url: string, options?: object) => void
+				openTelegramLink: (url: string) => void
+				openInvoice: (url: string, callback?: (status: string) => void) => void
+				onEvent: (eventType: string, callback: () => void) => void
+				offEvent: (eventType: string, callback: () => void) => void
+			}
+		}
+	}
+}
